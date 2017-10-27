3549cb14db45435cb8a3b58965144d964d4aed13
borodinchik <borodin.vladymyr@gmail.com>
2017-10-27 16:47:27 +0300

    last commit

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,12 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
         $this->call(AdminsTableSeeder::class);
         $this->call(QuestionsTableSeeder::class);
         $this->call(AnswereTableSeeder::class);
-
-
     }
 
 }
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\User;
 
 class UsersTableSeeder extends Seeder
 {
@@ -11,5 +12,54 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
+      User::create([
+        'name' => 'Александр',
+        'email' => 'sasha@gmail.com',
+        'password' => bcrypt('vladymyr'),
+        'phone_number' => '0969857563',
+        'your_age' => '1994-01-13',
+        'gender' => 'Man',
+      ]);
+      User::create([
+        'name' => 'Петр',
+        'email' => 'peyro@gmail.com',
+        'password' => bcrypt('123456'),
+        'phone_number' => '0969857489',
+        'your_age' => '1994-01-13',
+        'gender' => 'Man',
+      ]);
+      User::create([
+        'name' => 'Алексей',
+        'email' => 'alex@gmail.com',
+        'password' => bcrypt('123456'),
+        'phone_number' => '0969857552',
+        'your_age' => '1994-01-13',
+        'gender' => 'Man',
+      ]);
+      User::create([
+        'name' => 'Виктор',
+        'email' => 'vit@gmail.com',
+        'password' => bcrypt('123456'),
+        'phone_number' => '0969857745',
+        'your_age' => '1994-01-13',
+        'gender' => 'Man',
+      ]);
+      User::create([
+        'name' => 'Денис',
+        'email' => 'den@gmail.com',
+        'password' => bcrypt('123456'),
+        'phone_number' => '0969857589',
+        'your_age' => '1994-01-13',
+        'gender' => 'Man',
+      ]);
+      User::create([
+        'name' => 'Захар',
+        'email' => 'zahar@gmail.com',
+        'password' => bcrypt('123456'),
+        'phone_number' => '0969857147',
+        'your_age' => '1994-01-13',
+        'gender' => 'Man'
+      ]);
 
       }
+    }

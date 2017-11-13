d58b29de4cd28c326e44e792055c9feed580247e
borodinchik <borodin.vladymyr@gmail.com>
2017-11-13 07:43:52 +0200

    manday commyt

--- app/Http/Controllers/AnswerController.php
+++ app/Http/Controllers/AnswerController.php
@@ -17,19 +17,26 @@ public function getAnswer()
           }
 public function getInfoToGrap()
 {
-  
-  $data = UserAnswers::leftJoin('users', function($join) use($id) {
-  $join->on('users.id', '=', 'results.user_id');
+  // select count(user_id) as count_user_id from (
+  //   select questions.id as question_id,answers.id as answer_id,user_answers.user_id
+  //   from questions 
+  //   inner join answers on answers.question_id = questions.id
+  //   inner join user_answers on user_answers.user_answer_id = answers.id
+  //   where question_id = ?
+  //   ) as temp
+  //   group by temp.answer_id;
+  // $data = UserAnswers::leftJoin('users', function($join) use($id) {
+  // $join->on('users.id', '=', 'results.user_id');
 
-  })->leftJoin('answers', function($join) use($id) {
-  $join->on('results.answer_id', '=', 'answers.id');
+  // })->leftJoin('answers', function($join) use($id) {
+  // $join->on('results.answer_id', '=', 'answers.id');
 
-  })->selectRaw('results.question_id as question_id, results.answer_id as answer_id, users.id as user_id, 
-  answers.answer, users.sex, count(users.sex) as count_sex, count(users.date_of_birth) as count_bd')
-  ->groupBy(['answers.answer','users.sex'
-  ])->where('users.date_of_birth', '<=', '1999-01-01')->where('results.question_id', $id)
-  ->get()
-  ->toArray();
+  // })->selectRaw('results.question_id as question_id, results.answer_id as answer_id, users.id as user_id, 
+  // answers.answer, users.sex, count(users.sex) as count_sex, count(users.date_of_birth) as count_bd')
+  // ->groupBy(['answers.answer','users.sex'
+  // ])->where('users.date_of_birth', '<=', '1999-01-01')->where('results.question_id', $id)
+  // ->get()
+  // ->toArray();
   dd($data);
 
 

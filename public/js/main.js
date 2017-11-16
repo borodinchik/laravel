d1d7e7b33e6a6e78fd48d6a4118453413032b8b4
borodinchik <borodin.vladymyr@gmail.com>
2017-11-16 16:38:38 +0200

    Coomit Graph

--- public/js/main.js
+++ public/js/main.js
@@ -89,25 +89,25 @@ $(function () {
 //   alert(responseObj);
 
 //Лоика построенияграфика
-  var ctx = document.getElementById('myChart').getContext('2d');
-  var chart = new Chart(ctx, {
-    // The type of chart we want to create
-    type: 'bar',
-
-    // The data for our dataset
-    data: {
-        labels: ["Variant1", "Variant2", "Variant3", "Variant4", "Variant5", "Variant6", "Variant7"],
-        datasets: [{
-            label: "Название опроса",
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
-            data: [5,6,7,6,8,3,1,4]
-        }]
-    },
-
-    // Configuration options go here
-    options: {}
-});
+//   var ctx = document.getElementById('myChart').getContext('2d');
+//   var chart = new Chart(ctx, {
+//     // The type of chart we want to create
+//     type: 'bar',
+//
+//     // The data for our dataset
+//     data: {
+//         labels: ["Variant1", "Variant2", "Variant3", "Variant4", "Variant5", "Variant6", "Variant7"],
+//         datasets: [{
+//             label: "Название опроса",
+//             backgroundColor: 'rgb(255, 99, 132)',
+//             borderColor: 'rgb(255, 99, 132)',
+//             data: [5,6,7,6,8,3,1,4]
+//         }]
+//     },
+//
+//     // Configuration options go here
+//     options: {}
+// });
 
 //Достаемданные для построения графика
 // function getDataGraph(data){
